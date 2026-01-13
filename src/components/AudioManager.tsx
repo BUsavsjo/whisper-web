0617c1c8981d406b3a2a8e30d4b1a368a90875ea
Peter Wenström <peter.wenstrom@savsjo.se>
2026-01-13 09:46:26 +0100

    uppdaterat gdpr

--- src/components/AudioManager.tsx
+++ src/components/AudioManager.tsx
@@ -312,6 +312,16 @@ export function AudioManager(props: { transcriber: Transcriber }) {
                     </Trans>
                 }
             />
+            <InfoTile
+                className='fixed bottom-4 left-4'
+                icon={<ShieldIcon />}
+                title={t("manager.privacy_title")}
+                content={
+                    <Trans i18nKey='manager.privacy_content'>
+                        Privacy content
+                    </Trans>
+                }
+            />
             <SettingsTile
                 className='fixed bottom-4 right-4'
                 transcriber={props.transcriber}
@@ -863,6 +873,18 @@ function InfoIcon() {
             fill='currentColor'
         >
             <path d='M12 17q.425 0 .713-.288T13 16v-4q0-.425-.288-.712T12 11t-.712.288T11 12v4q0 .425.288.713T12 17m0-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8'></path>
+        </svg>
+    );
+}
+
+function ShieldIcon() {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+        >
+            <path d='M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z'></path>
         </svg>
     );
 }
